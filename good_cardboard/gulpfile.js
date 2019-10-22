72942153383923c77ab8311b72833fcc2f81ec61
Evgenii Solovev <solovyevmeh163@gmail.com>
2019-10-22 18:07:00 +0500

    inti gulp

---
+++ good_cardboard/gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+const bs = require('browser-sync');
+const sass = require('gulp-sass');
+var gutil = require( 'gulp-util' );
+var ftp = require( 'vinyl-ftp' );
+
+
+// Запускаем сервер, предварительно скопилировав SASS
+gulp.task('serve', ['sass'], () => {
+
+    bs.init({
+        server: "./src"
+    });
+
+    gulp.watch("src/sass/*.sass", ['sass']);
+    gulp.watch("src/*.html").on('change', bs.reload);
+});
+
+// Делаем компиляцию SASS в CSS 
+gulp.task('sass', () => {
+    return gulp.src("src/sass/*.sass")
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(bs.stream());
+});
+
+gulp.task('default', ['serve']);
+
+// Выгрузка проекта на FTP-сервер
+gulp.task('deploy', () => {
+
+	var conn = ftp.create( {
+		host:     '89.108.85.65',
+		user:     'ashto509',
+		password: 'wRaO91UPoc',
+		log:      gutil.log
+	} );
+
+	var globs = [
+		'src/**'
+	];
+
+	// using base = '.' will transfer everything to /public_html correctly
+	// turn off buffering in gulp.src for best performance
+
+	return gulp.src( globs, { base: '.', buffer: false } )
+		.pipe( conn.newer( '/www/evgeniisolovev.ru' ) ) // only upload newer files
+		.pipe( conn.dest( '/www/evgeniisolovev.ru' ) );
+
+} );
