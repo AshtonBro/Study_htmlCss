73d66daf78fa4b9dc74ada4462c9bd37c3edc9f0
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-02-18 22:58:21 +0500

    jQuery connect

--- good_cardboard/script/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.header-contacts__button').on('click', function(){
-        $('.overlay').show();
-    });
-    $('.popup-close').on('click', function(){
-        $('.overlay').hide();
-    });
-});
