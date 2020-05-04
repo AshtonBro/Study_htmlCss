6a2a142cbe5cb8e28927808121be5296973b5d13
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-05-04 22:58:06 +0500

    element before

--- good_cardboard/src/js/main.js
+++ good_cardboard/src/js/main.js
@@ -1,1 +1,10 @@
 'use strict';
+$(document).ready(function () {
+    $('.multiple-items').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true
+    });
+});
