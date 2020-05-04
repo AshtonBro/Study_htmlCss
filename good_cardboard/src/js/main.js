6bffb2aa5f3d2ea4aa7cfdd85ac4b137ba828158
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-05-05 02:01:41 +0500

    main screen is don

--- good_cardboard/src/js/main.js
+++ good_cardboard/src/js/main.js
@@ -1,10 +1,54 @@
 'use strict';
 $(document).ready(function () {
-    $('.multiple-items').slick({
-        slidesToShow: 3,
+    $('.main-slider').slick({
+        arrows: true,
+        slidesToShow: 6,
         slidesToScroll: 1,
-        autoplay: true,
+        prevArrow: '<div class="slider-arrow slider-arrow_left"></div>',
+        nextArrow: '<div class="slider-arrow slider-arrow_right"></div>',
+        autoplay: false,
         autoplaySpeed: 2000,
-        arrows: true
+        responsive: [{
+                breakpoint: 1200,
+                settings: {
+                    centerPadding: '60px',
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    centerPadding: '40px',
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    centerPadding: '40px',
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    centerPadding: '30px',
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    centerPadding: '30px',
+                    arrows: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+        ]
     });
 });
