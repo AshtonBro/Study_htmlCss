16518a0165a202178133943c71dedeaf11197402
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-05-06 22:30:20 +0500

    different param flowers

--- good_cardboard/gulpfile.js
+++ good_cardboard/gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const bs = require('browser-sync');
 const sass = require('gulp-sass');
-var gutil = require( 'gulp-util' );
-var ftp = require( 'vinyl-ftp' );
+var gutil = require('gulp-util');
+var ftp = require('vinyl-ftp');
 const autoprefixer = require('gulp-autoprefixer');
 //var fontgen = require('gulp-fontgen');
 //var ttf2woff = require('gulp-ttf2woff');
@@ -11,24 +11,25 @@ const autoprefixer = require('gulp-autoprefixer');
 // Запускаем сервер, предварительно скопилировав SASS
 gulp.task('serve', ['sass'], () => {
 
-    bs.init({
-        server: "./src"
-    });
+	bs.init({
+		server: "./src"
+	});
 
-    gulp.watch("src/sass/*.sass", ['sass']);
-    gulp.watch("src/*.html").on('change', bs.reload);
+	gulp.watch("src/sass/*.sass", ['sass']);
+	gulp.watch("src/*.html").on('change', bs.reload);
+	gulp.watch("src/js/*.js").on('change', bs.reload);
 });
 
 // Делаем компиляцию SASS в CSS 
 gulp.task('sass', () => {
-    return gulp.src("src/sass/*.sass")
+	return gulp.src("src/sass/*.sass")
 		.pipe(sass())
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest("src/css"))
-        .pipe(bs.stream());
+			cascade: false
+		}))
+		.pipe(gulp.dest("src/css"))
+		.pipe(bs.stream());
 });
 
 // Конвертируем шрифты
@@ -44,9 +45,9 @@ gulp.task('default', ['serve']);
 // Выгрузка проекта на FTP-сервер
 gulp.task('deploy', () => {
 
-	var conn = ftp.create( {
-		log:      gutil.log
-	} );
+	var conn = ftp.create({
+		log: gutil.log
+	});
 
 	var globs = [
 		'src/**'
@@ -55,8 +56,11 @@ gulp.task('deploy', () => {
 	// using base = '.' will transfer everything to /public_html correctly
 	// turn off buffering in gulp.src for best performance
 
-	return gulp.src( globs, { base: '.', buffer: false } )
-		.pipe( conn.newer( '/www/evgeniisolovev.ru' ) ) // only upload newer files
-		.pipe( conn.dest( '/www/evgeniisolovev.ru' ) );
+	return gulp.src(globs, {
+			base: '.',
+			buffer: false
+		})
+		.pipe(conn.newer('/www/evgeniisolovev.ru')) // only upload newer files
+		.pipe(conn.dest('/www/evgeniisolovev.ru'));
 
-} );
+});
--- good_cardboard/src/js/main.js
+++ good_cardboard/src/js/main.js
@@ -56,17 +56,107 @@ $(document).ready(function () {
 
 // * Work with the site on JavaScript
 const sliderImg = document.querySelectorAll('.main-slider-block__img'),
-    overlay = document.querySelector('.overlay'),
-    btnClose = document.querySelector('.popup-close');
+    overlay = document.querySelector('.overlay');
 
+// * Toggle function for add/remove class
 const toggleFunc = () => {
     overlay.classList.toggle('is-open');
+    findFlower();
 };
 
+// * local data base
+let obj = {
+    flower1: "Роза",
+    descr1: `Розы - это цветы не бывало популярны. На данный момент существует несколько тысяч сортов
+    роз. Розы это цветы, которые обладают волшебной красотой и притягательностью. Сложено много легенд о сказочной
+    розе. Ее любят все, ну и даже преклоняются пред ней. Все народы мира чествуют это прекрасное растение.`,
+
+    flower2: "Лилия",
+    descr2: `Цветок Лилия происхождением из Рима и Египта. Эти цветы были почитаемы еще в Древней Греции. Изначально они имели исключительно белую окраску, благодаря которой стали символизировать чистоту и нежность.`,
+
+    flower3: "Георгин",
+    descr3: `Племена майя использовали георгины для проведения своих ритуалов. Так называемые «храмы солнца» просто утопали в этих ярких цветах. В древности люди считали георгины посланниками Солнца, им поклонялись и почитали их. `,
+
+    flower4: "Сирень",
+    descr4: `Какая девушка не мечтает о том, чтобы ей весной подарили охапку сирени? Красивая, ароматная и невероятно романтичная сирень радует своим цветением. Многие знают о примете – если найти и съесть цветок сирени, на котором пять лепестков и загадать желание – оно непременно исполнится.`,
+
+    flower5: "Гербера",
+    descr5: `У каждого вида своя удивительная история путешествия, свои предпочтения и особенности. Среди таких «пришельцев-путешественников» можно встретить удивительную и многоликую герберу. Она символизирует силу и радость жизни, поэтому ее часто дарят на праздники.`,
+
+    flower6: "Азалия",
+    descr6: `Азалия — одно из самых красивых горшечных растений, цветущих в зимнее время. Крупных ярких цветков обычно бывает так много, что за ними практически теряется сам небольшой кустик с глянцевыми темно-зелеными листьями.`,
+
+    flower7: "Гербера фиолетовая",
+    descr7: `Согласно древним преданиям, георгин считался царским цветком, его выращивали только на территории княжеских садов.`
+};
+
+let flower = '';
+let descr = '';
+
+// * Creating a modal window with a description of the flower
+const createCardFlower = () => {
+    const card = document.createElement('div');
+    card.insertAdjacentHTML('beforeend', `
+        <div class="popup">
+            <div class="popup-title">${flower}</div>
+            <div class="popup-close">&times;</div>
+            <div class="popup-descr">${descr}
+            </div>
+        </div>
+    `);
+
+    overlay.insertAdjacentElement('beforeend', card);
+};
+
+// * Find the flower by target and assign parameters to it
+const findFlower = () => {
+    const targer = event.target;
+    console.log('targer: ', targer.alt);
+    if (targer.alt == 'flower-1') {
+        flower = obj.flower1;
+        descr = obj.descr1;
+        createCardFlower();
+    }
+    if (targer.alt == 'flower-2') {
+        flower = obj.flower2;
+        descr = obj.descr2;
+        createCardFlower();
+    }
+    if (targer.alt == 'flower-3') {
+        flower = obj.flower3;
+        descr = obj.descr3;
+        createCardFlower();
+    }
+    if (targer.alt == 'flower-4') {
+        flower = obj.flower4;
+        descr = obj.descr4;
+        createCardFlower();
+    }
+    if (targer.alt == 'flower-5') {
+        flower = obj.flower5;
+        descr = obj.descr5;
+        createCardFlower();
+    }
+    if (targer.alt == 'flower-6') {
+        flower = obj.flower6;
+        descr = obj.descr6;
+        createCardFlower();
+    }
+    if (targer.alt == 'flower-7') {
+        flower = obj.flower7;
+        descr = obj.descr7;
+        createCardFlower();
+    }
+};
 
 // * Event listeners
 sliderImg.forEach(element => {
     element.addEventListener('click', toggleFunc);
 });
-
-btnClose.addEventListener('click', toggleFunc);
+overlay.addEventListener('click', () => {
+    const targer = event.target;
+    if (targer.closest('.popup-close')) {
+        toggleFunc();
+        document.querySelector('.popup').remove();
+    }
+});
